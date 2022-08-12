8f0de1d46d853076ea91d496d6d9c38e636fa616
danacioDS <58677272+danacioDS@users.noreply.github.com>
2022-08-12 16:19:59 -0400

    Created by footer

--- mojix_bootcamp.py
+++ mojix_bootcamp.py
@@ -1,3 +1,21 @@
 import streamlit as st
+import pandas as pd
+import sqlalchemy
+
 
 st.write("Hello Mojix BootCamp")
+
+st.title('Fundamentals comparison')
+
+engine = sqlalchemy.create_engine('sqlite:///Fundamentals.db')
+df = pd.read_sql('Fundamentalstable',engine, index_col=['symbol'])
+
+dropdown_1 = st.selectbox('Choose your sector',
+df.sector.unique())
+
+dropdown_2 = st.selectbox('Choose your metric',
+df.columns[df.columns !='sector'])
+
+values = df[df.sector==dropdown_1][[dropdown_2]]
+
+st.bar_chart(values)
